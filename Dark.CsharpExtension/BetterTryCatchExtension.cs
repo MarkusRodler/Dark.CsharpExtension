67025556d123cbbc5b0b2cd76f29ee8aefecd5c0
Markus Rodler <Markus.Rodler@live.de>
2024-05-03 16:30:36 +0200

    ✨ Add BetterTryCatchExtension

---
+++ Dark.CsharpExtension/BetterTryCatchExtension.cs
@@ -0,0 +1,40 @@
+namespace Dark.CsharpExtension;
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Benennungsstile", Justification = "Does make more sense in this case")]
+public static class BetterTryCatchExtension
+{
+    public static Task Catch<E>(this Task task, Action<E> action) where E : Exception
+    {
+        if (task.Exception?.InnerException is null or not E) return task;
+
+        action.Invoke((E)task.Exception.InnerException);
+        return Task.CompletedTask;
+    }
+
+    public static Task<T> Catch<E, T>(this Task<T> task, Func<E, Task<T>> action) where E : Exception
+        => task.Exception?.InnerException is null or not E ? task : action((E)task.Exception.InnerException);
+
+    public static void Catch<E>(this Action task, Action<E> action) where E : Exception
+    {
+        try
+        {
+            task.Invoke();
+        }
+        catch (E e)
+        {
+            action.Invoke(e);
+        }
+    }
+
+    public static T Catch<E, T>(this Func<T> task, Func<E, T> action) where E : Exception
+    {
+        try
+        {
+            return task.Invoke();
+        }
+        catch (E e)
+        {
+            return action.Invoke(e);
+        }
+    }
+}
---
+++ Test/Dark.CsharpExtension/BetterTryCatchExtensionTest.cs
@@ -0,0 +1,90 @@
+using Dark.CsharpExtension;
+
+namespace Test;
+
+[TestClass]
+public class BetterTryCatchExtensionTest
+{
+    [TestMethod]
+    public async Task CatchCanCatchExceptionIfTypeMatches()
+    {
+        var failingTask = Task.FromException<string>(new IndexOutOfRangeException("Index was out of range"));
+        Action failingAction = () => throw new IndexOutOfRangeException("Index was out of range");
+        Func<string> failingFunction = () => throw new IndexOutOfRangeException("Index was out of range");
+        static Task<string> WillFail(string arg)
+            => Task.FromException<string>(new IndexOutOfRangeException("Index was out of range"));
+        Exception? catchedException1 = null;
+        Exception? catchedException2 = null;
+        Exception? catchedException3 = null;
+        Exception? catchedException4 = null;
+        Exception? catchedException5 = null;
+        Exception? catchedException6 = null;
+
+        await failingTask.Catch<IndexOutOfRangeException>(e => catchedException1 = e);
+        var result1 = await failingTask.Catch<IndexOutOfRangeException, string>(
+            (e) => { catchedException2 = e; return Task.FromResult("NOK"); }
+        );
+        failingAction.Catch<IndexOutOfRangeException>(e => catchedException3 = e);
+        var result2 = failingFunction.Catch<IndexOutOfRangeException, object>(
+            e => { catchedException4 = e; return "NOK"; }
+        );
+        await WillFail("Hello?").Catch((IndexOutOfRangeException e) => catchedException5 = e);
+        var result3 = await WillFail("Hello?").Catch(
+            (IndexOutOfRangeException e) => { catchedException6 = e; return Task.FromResult("NOK"); }
+        );
+
+        Assert.IsInstanceOfType<IndexOutOfRangeException>(catchedException1);
+        Assert.AreEqual("Index was out of range", catchedException1.Message);
+        Assert.IsInstanceOfType<IndexOutOfRangeException>(catchedException2);
+        Assert.AreEqual("Index was out of range", catchedException2.Message);
+        Assert.IsInstanceOfType<IndexOutOfRangeException>(catchedException3);
+        Assert.AreEqual("Index was out of range", catchedException3.Message);
+        Assert.IsInstanceOfType<IndexOutOfRangeException>(catchedException4);
+        Assert.AreEqual("Index was out of range", catchedException4.Message);
+        Assert.IsInstanceOfType<IndexOutOfRangeException>(catchedException5);
+        Assert.AreEqual("Index was out of range", catchedException5.Message);
+        Assert.IsInstanceOfType<IndexOutOfRangeException>(catchedException6);
+        Assert.AreEqual("Index was out of range", catchedException6.Message);
+        Assert.AreEqual("NOK", result1);
+        Assert.AreEqual("NOK", result2);
+        Assert.AreEqual("NOK", result3);
+    }
+
+    [TestMethod]
+    public async Task CatchWillNotExecuteActionIfNoExceptionIsThrown()
+    {
+        var task = Task.FromResult("Success");
+        Action nonFailingAction = () => { return; };
+        Func<string> nonFailingFunc = () => "Success";
+        static Task<string> WillNotFail(string arg) => Task.FromResult("Hello " + arg);
+        Exception? catchedException1 = null;
+        Exception? catchedException2 = null;
+        Exception? catchedException3 = null;
+        Exception? catchedException4 = null;
+        Exception? catchedException5 = null;
+        Exception? catchedException6 = null;
+
+        await task.Catch<IndexOutOfRangeException>(e => catchedException1 = e);
+        var result1 = await task.Catch<IndexOutOfRangeException, string>(
+            e => { catchedException2 = e; return Task.FromResult("NOK"); }
+        );
+        nonFailingAction.Catch<IndexOutOfRangeException>(e => catchedException3 = e);
+        var result2 = nonFailingFunc.Catch<IndexOutOfRangeException, string>(
+            e => { catchedException4 = e; return "NOK"; }
+        );
+        await WillNotFail("World").Catch((IndexOutOfRangeException e) => catchedException5 = e);
+        var result3 = await WillNotFail("World").Catch(
+            (IndexOutOfRangeException e) => { catchedException6 = e; return Task.FromResult("NOK"); }
+        );
+
+        Assert.IsNull(catchedException1);
+        Assert.IsNull(catchedException2);
+        Assert.IsNull(catchedException3);
+        Assert.IsNull(catchedException4);
+        Assert.IsNull(catchedException5);
+        Assert.IsNull(catchedException6);
+        Assert.AreEqual("Success", result1);
+        Assert.AreEqual("Success", result2);
+        Assert.AreEqual("Hello World", result3);
+    }
+}
